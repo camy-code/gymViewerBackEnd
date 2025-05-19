ac974a53b806e37daf98c10f7b456b313dd117c4
camden.warburton <camden.warburton@ucalgary.ca>
2025-05-19 12:18:54 -0600

    Made a basic server be able to go to flask

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 # following the tutorial
-    # https://www.youtube.com/watch?v=7LNl2JlZKHA
+# https://www.youtube.com/watch?v=7LNl2JlZKHA
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/activities")
 def members():
-    return {"members":["ONE","TWO","THREE"]}
+    return {"members": ["ONE", "TWO", "THREE"]}
 
 
 if __name__ == "__main__":
