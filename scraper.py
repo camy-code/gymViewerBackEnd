2031cb5e1f87e835a800b3c51e9e022924885654
camden.warburton <camden.warburton@ucalgary.ca>
2025-05-19 15:08:59 -0600

    Got a basic constant format

--- scraper.py
+++ scraper.py
@@ -14,4 +14,27 @@
 
 def dum_scrape():
     # Time to organzie
-    return ["one", "two", "three"]
+    myLs = []
+
+    days = ["Wednesday, April 30", "Thursday, May 1"] # This is just a loop for all days
+    for day in days:
+        pass
+        temp_dict = dict()
+        # 1. Do the name
+        temp_dict["name"] = day
+
+        # 2. Do expand
+        temp_dict["expand"] = False
+
+        # 3. Do activities
+        temp_act = []
+            # Do some work here later
+        temp_act.append( {"color":"#E0E0E0", "gym":"red gym", "sport":"hockey", "time":"12-1pm"})
+
+        temp_dict["activities"] = temp_act
+
+
+        myLs.append(temp_dict)
+
+    # return ["one", "two", "three"]
+    return myLs
